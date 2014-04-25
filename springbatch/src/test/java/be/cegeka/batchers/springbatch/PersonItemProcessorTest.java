fc2c503dbc7a401e95ab8af16c57d4759e639464
Silviu Catarau <freemanfx@gmail.com>
2014-04-25 10:39:31 +0300

    added sources generated from archetype https://github.com/chrisjs/maven-springbatch-archetype

---
+++ springbatch/src/test/java/be/cegeka/batchers/springbatch/PersonItemProcessorTest.java
@@ -0,0 +1,24 @@
+package be.cegeka.batchers.springbatch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <p>
+ * Example test case transforming a {@link Person} using the {@link PersonItemProcessor}.
+ * </p>
+ */
+public class PersonItemProcessorTest {
+    @Test
+    public void testProcessedPersonRecord() throws Exception {
+        Person person = new Person();
+        person.setFirstName("Jane");
+        person.setLastName("Doe");
+
+        Person processedPerson = new PersonItemProcessor().process(person);
+
+        assertEquals("JANE", processedPerson.getFirstName());
+        assertEquals("DOE", processedPerson.getLastName());
+    }
+}
