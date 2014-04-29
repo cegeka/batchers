69b15472abff7bc69ed5b89e4322519dbde6ad9d
Radu Cirstoiu <radu.cirstoiu@cegeka.com>
2014-04-29 14:18:09 +0300

    service working without text file log

---
+++ taxservice/src/main/java/be/cegeka/batchers/taxservice/TaxTo.java
@@ -0,0 +1,42 @@
+package be.cegeka.batchers.taxservice;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: raduci
+ * Date: 29.04.2014
+ * Time: 12:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class TaxTo {
+
+    @NotNull
+    private String employeeId;
+
+    @NotNull
+    private Double amount;
+
+    public TaxTo() {}
+
+    public TaxTo(String employeeId, Double amount) {
+        this.employeeId = employeeId;
+        this.amount = amount;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
