ae514630f0af6014b2a59b6bf447049bf69e170e
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-04-29 14:15:39 +0300

    edward, andrei created EmployeeReader

---
+++ springbatch/src/main/java/be/cegeka/batchers/springbatch/jobs/employeetax/EmployeeReader.java
@@ -0,0 +1,39 @@
+package be.cegeka.batchers.springbatch.jobs.employeetax;
+
+import be.cegeka.batchers.springbatch.domain.Employee;
+import be.cegeka.batchers.springbatch.domain.EmployeeRepository;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Iterator;
+
+/**
+ * Created by andreip on 29.04.2014.
+ */
+public class EmployeeReader implements ItemReader<Employee> {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    private Iterator<Employee> itemIterator;
+
+    @Override
+    public Employee read() {
+        if(getItemIterator().hasNext()){
+            return getItemIterator().next();
+        } else {
+            return null;
+        }
+    }
+
+    private Iterator<Employee> getItemIterator() {
+        if (itemIterator == null) {
+            itemIterator = employeeRepository.getAll().iterator();
+        }
+        return itemIterator;
+    }
+
+    void setEmployeeRepo(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+}
