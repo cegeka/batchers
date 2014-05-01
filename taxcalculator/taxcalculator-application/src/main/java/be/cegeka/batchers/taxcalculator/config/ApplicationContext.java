400b422e2702f6ac33a5e93bc75a51b57d785369
Silviu Catarau <freemanfx@gmail.com>
2014-05-01 17:35:42 +0300

    [silviu] added EmployeeRestController

--- taxcalculator/taxcalculator-application/src/main/java/be/cegeka/batchers/taxcalculator/config/ApplicationContext.java
+++ taxcalculator/taxcalculator-application/src/main/java/be/cegeka/batchers/taxcalculator/config/ApplicationContext.java
@@ -8,9 +8,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableTransactionManagement
+@EnableWebMvc
 @Import(PersistenceConfig.class)
 @ComponentScan(basePackages = "be.cegeka.batchers.taxcalculator")
 public class ApplicationContext {
