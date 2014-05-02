d60576473ce9ad234f2964fad33480d5c28aa56a
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-05-02 18:08:05 +0300

    andrei fixed unit test

--- taxcalculator/taxcalculator-batch/src/test/java/be/cegeka/batchers/taxcalculator/EmployeeJobTest.java
+++ taxcalculator/taxcalculator-batch/src/test/java/be/cegeka/batchers/taxcalculator/EmployeeJobTest.java
@@ -1,5 +1,6 @@
 package be.cegeka.batchers.taxcalculator;
 
+import be.cegeka.batchers.taxcalculator.domain.Employee;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
@@ -11,13 +12,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.Date;
 
 import static org.junit.Assert.assertTrue;
 
 @ContextConfiguration("employeeJobTest-context.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 public class EmployeeJobTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -28,6 +38,12 @@ public class EmployeeJobTest {
     @Autowired
     private Job job;
 
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
     @Test
     public void testEmployeeJob() throws Exception {
         JobExecution jobExecution = jobLauncher.run(job, new JobParametersBuilder()
@@ -36,12 +52,19 @@ public class EmployeeJobTest {
 
         assertTrue("Batch status must be COMPLETED", jobExecution.getStatus() == BatchStatus.COMPLETED);
 
-        int numRows = jdbcTemplate.queryForObject("select count(*) from people", Integer.class);
-        assertTrue("Invalid number of rows found, expected 5 got: " + numRows, numRows == 5);
+        CriteriaBuilder qb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> cq = qb.createQuery(Long.class);
+        cq.select(qb.count(cq.from(Employee.class)));
+        Long numRows = entityManager.createQuery(cq).getSingleResult();
 
-        int numTransformedRows = jdbcTemplate.queryForObject("select count(*) from people where last_name='DOE'",
-                Integer.class);
-        assertTrue("Invalid number of transformed rows found, expected 5 got: " + numTransformedRows,
-                numTransformedRows == 5);
+        assertTrue("Invalid number of rows found, expected 300 got: " + numRows, numRows == 300);
+
+        TypedQuery<Employee> query =
+                entityManager.createQuery("SELECT e FROM Employee e", Employee.class).setMaxResults(1);
+        Employee employee = query.getSingleResult();
+
+        assertTrue("Employee has name not upeprcase " + employee.getLastName(),
+                employee.getLastName().toUpperCase().equals(employee.getLastName()));
+
     }
 }
