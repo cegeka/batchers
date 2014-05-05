5373b74bfd0381f200bec21fde35b4ca66561970
Silviu Catarau <freemanfx@gmail.com>
2014-05-05 11:28:17 +0300

    [silviu] added EmployeeServiceTest to test suite

--- taxcalculator/taxcalculator-application/src/test/java/be/cegeka/batchers/taxcalculator/ApplicationTestSuite.java
+++ taxcalculator/taxcalculator-application/src/test/java/be/cegeka/batchers/taxcalculator/ApplicationTestSuite.java
@@ -2,6 +2,7 @@ package be.cegeka.batchers.taxcalculator;
 
 import be.cegeka.batchers.taxcalculator.domain.EmployeeGeneratorTest;
 import be.cegeka.batchers.taxcalculator.domain.EmployeeRepositoryTest;
+import be.cegeka.batchers.taxcalculator.domain.EmployeeServiceTest;
 import be.cegeka.batchers.taxcalculator.rest.EmployeeRestControllerTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -9,6 +10,6 @@ import org.junit.runners.Suite;
 import static org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses(value = {EmployeeGeneratorTest.class, EmployeeRepositoryTest.class, EmployeeRestControllerTest.class})
+@SuiteClasses(value = {EmployeeGeneratorTest.class, EmployeeRepositoryTest.class, EmployeeRestControllerTest.class, EmployeeServiceTest.class})
 public class ApplicationTestSuite {
 }
