e04dbf81cad47ee4c8263f6ed3f8adff8c972369
MoniQQ <monica.turiac@gmail.com>
2014-05-08 15:20:58 +0300

    Monica, Silviu: use taxcalculator-to in batch to call webservice.

--- taxcalculator/taxcalculator-batch/src/main/java/be/cegeka/batchers/taxcalculator/batch/CallWebserviceProcessor.java
+++ taxcalculator/taxcalculator-batch/src/main/java/be/cegeka/batchers/taxcalculator/batch/CallWebserviceProcessor.java
@@ -1,9 +1,10 @@
 package be.cegeka.batchers.taxcalculator.batch;
 
 import be.cegeka.batchers.taxcalculator.application.domain.Employee;
+import be.cegeka.batchers.taxcalculator.to.TaxTo;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -21,8 +22,12 @@ public class CallWebserviceProcessor implements ItemProcessor<Employee, Employee
 
     @Override
     public Employee process(Employee employee) throws Exception {
-        ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity(URI.create(taxServiceUrl), null, String.class);
-        if (stringResponseEntity.getBody().equals("OK")) return employee;
+        TaxTo taxTo = new TaxTo();
+        taxTo.setAmount(employee.getIncomeTax());
+        taxTo.setEmployeeId(String.valueOf(employee.getId()));
+        ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity(URI.create(taxServiceUrl), taxTo, String.class);
+        if ("OK".equals(stringResponseEntity.getBody()))
+            return employee;
         throw new RuntimeException("Woops");
     }
 }
