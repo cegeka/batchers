4a2f91b982ffabc72c87df0236793009adc1baf9
Edward Gabriel Moraru <EdwardGabriel.Moraru@cegeka.be>
2014-05-12 09:41:39 +0300

    added scripts to run batch (for bash and for windows)

---
+++ taxcalculator/taxcalculator-batch/run-job.bat
@@ -0,0 +1,1 @@
+java -cp target/taxcalculator-batch-1.0-SNAPSHOT-jar-with-dependencies.jar org.springframework.batch.core.launch.support.CommandLineJobRunner be.cegeka.batchers.taxcalculator.batch.EmployeeJobConfig employeeJob
