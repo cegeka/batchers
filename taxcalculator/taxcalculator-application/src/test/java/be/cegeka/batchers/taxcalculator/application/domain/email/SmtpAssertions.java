29c7505c910b86f62d82c0cb80eeae9d23c8d534
Radu Cirstoiu <radu.cirstoiu@cegeka.com>
2014-05-13 15:21:07 +0300

    [Radu,Ronald] S14: added email sending and testing

---
+++ taxcalculator/taxcalculator-application/src/test/java/be/cegeka/batchers/taxcalculator/application/domain/email/SmtpAssertions.java
@@ -0,0 +1,59 @@
+package be.cegeka.batchers.taxcalculator.application.domain.email;
+
+
+import org.fest.assertions.api.AbstractAssert;
+import org.fest.assertions.api.Assertions;
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
+
+import java.util.List;
+
+public class SmtpAssertions extends AbstractAssert<SmtpAssertions, Wiser> {
+
+    public SmtpAssertions(Wiser actual) {
+        super(actual, SmtpAssertions.class);
+    }
+
+    public SmtpAssertions hasReceivedMessages(int numberOfMessages) {
+        Assertions.assertThat(actual.getMessages()).hasSize(numberOfMessages);
+        return this;
+    }
+
+    public SmtpAssertions hasReceivedMessageContaining(String text) {
+        List<WiserMessage> messages = actual.getMessages();
+        for (WiserMessage message : messages) {
+            if(new String(message.getData()).contains(text)) {
+                return this;
+            }
+        }
+        Assertions.fail("Could not find a message containt text '" + text + "'");
+        return this;
+    }
+
+    public SmtpAssertions hasReceivedMessageSentTo(String receiver) {
+        List<WiserMessage> messages = actual.getMessages();
+        for (WiserMessage message : messages) {
+            if(message.getEnvelopeReceiver().equals(receiver)) {
+                return this;
+            }
+        }
+        Assertions.fail("Could not find a message containt sent to '" + receiver + "'");
+        return this;
+    }
+
+    public SmtpAssertions hasReceivedMessageSentFrom(String sender) {
+        List<WiserMessage> messages = actual.getMessages();
+        for (WiserMessage message : messages) {
+            if(message.getEnvelopeSender().equals(sender)) {
+                return this;
+            }
+        }
+        Assertions.fail("Could not find a message containt sent to '" + sender + "'");
+        return this;
+    }
+
+    public SmtpAssertions hasNoReceivedMessages() {
+        hasReceivedMessages(0);
+        return this;
+    }
+}
