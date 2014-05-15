d07a610e67fee8e5664937f3fec5c18db4c53aee
MoniQQ <monica.turiac@gmail.com>
2014-05-15 14:12:00 +0300

    [Monica] Use Custom serializer for Money. Remove unused properties from application.

---
+++ taxcalculator/taxcalculator-application/src/main/java/be/cegeka/batchers/taxcalculator/application/util/jackson/JodaMoneySerializer.java
@@ -0,0 +1,17 @@
+package be.cegeka.batchers.taxcalculator.application.util.jackson;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.joda.money.Money;
+
+import java.io.IOException;
+
+public class JodaMoneySerializer extends JsonSerializer<Money> {
+
+    @Override
+    public void serialize(Money value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+        jgen.writeString(value.getAmount() + " €");
+    }
+}
