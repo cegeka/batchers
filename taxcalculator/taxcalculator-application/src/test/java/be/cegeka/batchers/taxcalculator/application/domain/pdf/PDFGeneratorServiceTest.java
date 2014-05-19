6696a04ee35a8f4752c6c0d06b3453aa84db8363
Silviu Catarau <freemanfx@gmail.com>
2014-05-19 09:59:22 +0300

    [Silviu] fix test

--- taxcalculator/taxcalculator-application/src/test/java/be/cegeka/batchers/taxcalculator/application/domain/pdf/PDFGeneratorServiceTest.java
+++ taxcalculator/taxcalculator-application/src/test/java/be/cegeka/batchers/taxcalculator/application/domain/pdf/PDFGeneratorServiceTest.java
@@ -3,7 +3,7 @@ package be.cegeka.batchers.taxcalculator.application.domain.pdf;
 import fr.opensagres.xdocreport.core.XDocReportException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.junit.Test;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -23,7 +23,7 @@ public class PDFGeneratorServiceTest {
         Map<String, Object> context = new HashMap<>();
         context.put("test", "cegeka-batchers");
 
-        byte[] actual = pdfGeneratorService.generatePdfAsByteArray(new FileSystemResource("src/test/resources/test_template.docx"), context);
+        byte[] actual = pdfGeneratorService.generatePdfAsByteArray(new ClassPathResource("test_template.docx"), context);
 
         assertThat(PDDocument.load(new ByteArrayInputStream(actual)))
                 .containsText("cegeka-batchers is working");
