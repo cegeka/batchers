c19273c08e1055f608dccb4fd0e4a36d48201a6f
tombr <tombr@HI09618.cegekanv.corp.local>
2014-05-26 11:49:59 +0200

    [tom] unused imports

--- taxcalculator/taxcalculator-application/src/main/java/be/cegeka/batchers/taxcalculator/application/service/TaxCalculatorService.java
+++ taxcalculator/taxcalculator-application/src/main/java/be/cegeka/batchers/taxcalculator/application/service/TaxCalculatorService.java
@@ -1,30 +1,30 @@
 package be.cegeka.batchers.taxcalculator.application.service;
 
-import be.cegeka.batchers.taxcalculator.application.domain.Employee;
-import be.cegeka.batchers.taxcalculator.application.domain.TaxCalculation;
+import java.math.RoundingMode;
+
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
-import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.RoundingMode;
+import be.cegeka.batchers.taxcalculator.application.domain.Employee;
+import be.cegeka.batchers.taxcalculator.application.domain.TaxCalculation;
 
 @Service
 public class TaxCalculatorService {
 
-    @Autowired
-    private RunningTimeService runningTimeService;
+	@Autowired
+	private RunningTimeService runningTimeService;
 
-    public TaxCalculation calculateTax(long jobExecutionId, Employee employee, long year, long month) {
-        runningTimeService.sleep();
-        double taxAmount = employee.getIncome() * 0.1;
-        Money tax = Money.of(CurrencyUnit.EUR, taxAmount, RoundingMode.HALF_DOWN);
+	public TaxCalculation calculateTax(long jobExecutionId, Employee employee, long year, long month) {
+		runningTimeService.sleep();
+		double taxAmount = employee.getIncome() * 0.1;
+		Money tax = Money.of(CurrencyUnit.EUR, taxAmount, RoundingMode.HALF_DOWN);
 
-        return TaxCalculation.from(jobExecutionId, employee, year, month, tax);
-    }
+		return TaxCalculation.from(jobExecutionId, employee, year, month, tax);
+	}
 
-    public void setRunningTimeService(RunningTimeService runningTimeService) {
-        this.runningTimeService = runningTimeService;
-    }
+	public void setRunningTimeService(RunningTimeService runningTimeService) {
+		this.runningTimeService = runningTimeService;
+	}
 }
