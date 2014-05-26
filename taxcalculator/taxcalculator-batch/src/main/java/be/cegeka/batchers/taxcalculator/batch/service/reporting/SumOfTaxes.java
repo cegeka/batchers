b4a07f071575c398098f423d3bc0fa762a08db43
tombr <tombr@HI09618.cegekanv.corp.local>
2014-05-26 11:48:54 +0200

    [tom] unused imports + serialVersionId for exception.

--- taxcalculator/taxcalculator-batch/src/main/java/be/cegeka/batchers/taxcalculator/batch/service/reporting/SumOfTaxes.java
+++ taxcalculator/taxcalculator-batch/src/main/java/be/cegeka/batchers/taxcalculator/batch/service/reporting/SumOfTaxes.java
@@ -1,21 +1,20 @@
 package be.cegeka.batchers.taxcalculator.batch.service.reporting;
 
-import be.cegeka.batchers.taxcalculator.application.domain.TaxServiceCallResultRepository;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import be.cegeka.batchers.taxcalculator.application.domain.TaxServiceCallResultRepository;
 
 @Component
 public class SumOfTaxes {
-    @Autowired
-    TaxServiceCallResultRepository taxServiceCallResultRepository;
+	@Autowired
+	TaxServiceCallResultRepository taxServiceCallResultRepository;
 
-    public double getSuccessSum(long year, long month) {
-        return taxServiceCallResultRepository.getSuccessSum(year, month).getAmount().doubleValue();
-    }
+	public double getSuccessSum(long year, long month) {
+		return taxServiceCallResultRepository.getSuccessSum(year, month).getAmount().doubleValue();
+	}
 
-    public double getFailedSum(long year, long month) {
-        return taxServiceCallResultRepository.getFailedSum(year, month).getAmount().doubleValue();
-    }
+	public double getFailedSum(long year, long month) {
+		return taxServiceCallResultRepository.getFailedSum(year, month).getAmount().doubleValue();
+	}
 }
--- taxcalculator/taxcalculator-batch/src/test/java/be/cegeka/batchers/taxcalculator/batch/EmployeeProcessorTest.java
+++ taxcalculator/taxcalculator-batch/src/test/java/be/cegeka/batchers/taxcalculator/batch/EmployeeProcessorTest.java
@@ -1,10 +1,9 @@
 package be.cegeka.batchers.taxcalculator.batch;
 
-import be.cegeka.batchers.taxcalculator.application.domain.Employee;
-import be.cegeka.batchers.taxcalculator.application.domain.EmployeeBuilder;
-import be.cegeka.batchers.taxcalculator.application.domain.TaxCalculation;
-import be.cegeka.batchers.taxcalculator.application.service.RunningTimeService;
-import be.cegeka.batchers.taxcalculator.application.service.TaxCalculatorService;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 import org.joda.time.DateTime;
@@ -15,63 +14,56 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.internal.util.reflection.Whitebox;
 import org.mockito.runners.MockitoJUnitRunner;
-
 import org.springframework.batch.core.StepExecution;
 
-import static org.fest.assertions.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import be.cegeka.batchers.taxcalculator.application.domain.Employee;
+import be.cegeka.batchers.taxcalculator.application.domain.EmployeeBuilder;
+import be.cegeka.batchers.taxcalculator.application.domain.TaxCalculation;
+import be.cegeka.batchers.taxcalculator.application.service.RunningTimeService;
+import be.cegeka.batchers.taxcalculator.application.service.TaxCalculatorService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EmployeeProcessorTest {
 
-    public static final double DELTA = 1e-15;
-    private Interval interval;
+	public static final double DELTA = 1e-15;
+	private Interval interval;
 
-    @InjectMocks
-    private CalculateTaxProcessor calculateTaxProcessor;
+	@InjectMocks
+	private CalculateTaxProcessor calculateTaxProcessor;
 
-    @Mock
-    private StepExecution stepExecution;
+	@Mock
+	private StepExecution stepExecution;
 
-    @Before
-    public void setUp() {
-        calculateTaxProcessor.taxCalculatorService = createTaxCalculatorService();
-        DateTime now = new DateTime();
-        LocalDate today = now.toLocalDate();
-        LocalDate tomorrow = today.plusDays(1);
-        interval = new Interval(today.toDateTimeAtStartOfDay(), tomorrow.toDateTimeAtStartOfDay());
+	@Before
+	public void setUp() {
+		calculateTaxProcessor.taxCalculatorService = createTaxCalculatorService();
+		DateTime now = new DateTime();
+		LocalDate today = now.toLocalDate();
+		LocalDate tomorrow = today.plusDays(1);
+		interval = new Interval(today.toDateTimeAtStartOfDay(), tomorrow.toDateTimeAtStartOfDay());
 
-        when(stepExecution.getJobExecutionId()).thenReturn(1L);
-    }
+		when(stepExecution.getJobExecutionId()).thenReturn(1L);
+	}
 
-    @Test
-    public void whenAnEmployeeWithoutCalculatedTax_isProcessed_thenTaxIsOnlyPercentOfCurrentIncome() throws Exception {
-        Employee employee1 = new EmployeeBuilder()
-                .withIncome(1000)
-                .withId(1L)
-                .build();
-        Employee employee2 = new EmployeeBuilder()
-                .withIncome(1500)
-                .withId(1L)
-                .build();
+	@Test
+	public void whenAnEmployeeWithoutCalculatedTax_isProcessed_thenTaxIsOnlyPercentOfCurrentIncome() throws Exception {
+		Employee employee1 = new EmployeeBuilder().withIncome(1000).withId(1L).build();
+		Employee employee2 = new EmployeeBuilder().withIncome(1500).withId(1L).build();
 
-        TaxCalculation taxCalculation1 = calculateTaxProcessor.process(employee1);
-        TaxCalculation taxCalculation2 = calculateTaxProcessor.process(employee2);
+		TaxCalculation taxCalculation1 = calculateTaxProcessor.process(employee1);
+		TaxCalculation taxCalculation2 = calculateTaxProcessor.process(employee2);
 
-        assertThat(taxCalculation1.getTax()).isEqualTo(Money.of(CurrencyUnit.EUR, 100));
-        assertThat(taxCalculation1.getCalculationDate()).isNotNull();
+		assertThat(taxCalculation1.getTax()).isEqualTo(Money.of(CurrencyUnit.EUR, 100));
+		assertThat(taxCalculation1.getCalculationDate()).isNotNull();
 
-        assertThat(taxCalculation2.getTax()).isEqualTo(Money.of(CurrencyUnit.EUR, 150));
-        assertThat(taxCalculation2.getCalculationDate()).isNotNull();
-    }
+		assertThat(taxCalculation2.getTax()).isEqualTo(Money.of(CurrencyUnit.EUR, 150));
+		assertThat(taxCalculation2.getCalculationDate()).isNotNull();
+	}
 
-
-    public TaxCalculatorService createTaxCalculatorService() {
-        TaxCalculatorService taxCalculatorService = new TaxCalculatorService();
-        taxCalculatorService.setRunningTimeService(mock(RunningTimeService.class));
-        return taxCalculatorService;
-    }
+	public TaxCalculatorService createTaxCalculatorService() {
+		TaxCalculatorService taxCalculatorService = new TaxCalculatorService();
+		taxCalculatorService.setRunningTimeService(mock(RunningTimeService.class));
+		return taxCalculatorService;
+	}
 }
