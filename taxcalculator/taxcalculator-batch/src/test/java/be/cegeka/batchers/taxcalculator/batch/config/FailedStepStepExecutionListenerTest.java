bbc7604618d4ec8ba1894323a81b0e293d032bad
Silviu Catarau <freemanfx@gmail.com>
2014-05-28 12:15:35 +0300

    [Silviu] fix job not failing when WS call step fails for at least one employee

---
+++ taxcalculator/taxcalculator-batch/src/test/java/be/cegeka/batchers/taxcalculator/batch/config/FailedStepStepExecutionListenerTest.java
@@ -0,0 +1,43 @@
+package be.cegeka.batchers.taxcalculator.batch.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FailedStepStepExecutionListenerTest {
+
+    @InjectMocks
+    private FailedStepStepExecutionListener failedStepStepExecutionListener;
+
+    @Mock
+    private JobExecution jobExecution;
+    private StepExecution stepExecution = new StepExecution("step", jobExecution);
+
+    @Test
+    public void testAfterStep_returns_FailedExitStatus_whenRead_And_Write_Count_Dont_Match() throws Exception {
+        stepExecution.setReadCount(2);
+        stepExecution.setWriteCount(1);
+
+        ExitStatus exitStatus = failedStepStepExecutionListener.afterStep(stepExecution);
+
+        assertThat(exitStatus).isEqualTo(ExitStatus.FAILED);
+    }
+
+    @Test
+    public void testAfterStep_returns_CompletedExitStatus_whenRead_And_Write_Match() throws Exception {
+        stepExecution.setReadCount(2);
+        stepExecution.setWriteCount(2);
+
+        ExitStatus exitStatus = failedStepStepExecutionListener.afterStep(stepExecution);
+
+        assertThat(exitStatus).isEqualTo(ExitStatus.COMPLETED);
+    }
+}
