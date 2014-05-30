7819601ec5b6d684ac773ad99361ace87e7ee9be
Silviu Catarau <freemanfx@gmail.com>
2014-05-30 15:34:35 +0300

    [Silviu,Radu]  clean-up

--- taxcalculator/taxcalculator-presentation/src/main/webapp/resources/js/taxcalculator-controllers.js
+++ taxcalculator/taxcalculator-presentation/src/main/webapp/resources/js/taxcalculator-controllers.js
@@ -2,53 +2,37 @@ var taxcalculatorControllers = angular.module('taxcalculatorControllers', []);
 
 taxcalculatorControllers
 
-    .controller('EmployeesOverviewCtrl', ['$scope', 'EmployeesOverviewResource',
-        function ($scope, EmployeesOverviewResource) {
-            $scope.employees = EmployeesOverviewResource.query(
-                {},
-                function (successData) {
-                },
-                function (error) {
-                    $scope.$emit("alert", {'alertClass': 'alert-danger', 'message': 'Could not retrieve application overview'})
-                }
-            );
+  .controller('EmployeesOverviewCtrl', ['$scope', 'EmployeesOverviewResource',
+    function ($scope, EmployeesOverviewResource) {
+      $scope.employees = EmployeesOverviewResource.query(
+        {},
+        function (successData) {
+        },
+        function (error) {
+          $scope.$emit("alert", {'alertClass': 'alert-danger', 'message': 'Could not retrieve application overview'})
         }
-    ])
-
-    .controller('RunJobCtrl', ['$scope', 'RunJobResource',
-        function ($scope, RunJobResource) {
-
-          $scope.runJob = function (year, month) {
-            RunJobResource.runJob(
-              {year: year, month: month},
-                function (successData) {
-                },
-                function (error) {
-                    $scope.$emit("alert", {'alertClass': 'alert-danger', 'message': 'Could not start job'})
-                }
-            );
-          }
-        }
-    ])
+      );
+    }
+  ])
 
   .controller('JobResultsCtrl', ['$scope', 'JobResultsResource', 'RunJobResource',
     function ($scope, JobResultsResource, RunJobResource) {
-            $scope.formatDuration = function (millis) {
-                if (millis === null) return "NOT FINISHED";
-                duration = moment.duration(millis);
-                return duration.hours() + "h " + duration.minutes() + "m " + duration.seconds() + "s " + duration.milliseconds() + "ms"
-            }
-            $scope.jobResults = JobResultsResource.query(
-                {},
-                function (successData) {
-                },
-                function (error) {
-                    $scope.$emit("alert", {'alertClass': 'alert-danger', 'message': 'Could not start job'})
-                }
-            );
+      $scope.formatDuration = function (millis) {
+        if (millis === null) return "NOT FINISHED";
+        duration = moment.duration(millis);
+        return duration.hours() + "h " + duration.minutes() + "m " + duration.seconds() + "s " + duration.milliseconds() + "ms"
+      }
+      $scope.jobResults = JobResultsResource.query(
+        {},
+        function (successData) {
+        },
+        function (error) {
+          $scope.$emit("alert", {'alertClass': 'alert-danger', 'message': 'Could not start job'})
+        }
+      );
 
       $scope.runJob = function (job) {
         RunJobResource.run({year: job.jobStartParams.year, month: job.jobStartParams.month});
       }
-        }
-    ])
+    }
+  ])
