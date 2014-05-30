d6542ceeecac7814aa4c23fa2a7cf19ecc1ea43f
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-05-30 14:01:15 +0300

    [Madalin, Andrei] removed redundant test

--- taxcalculator/taxcalculator-presentation/src/test/js/e2e/e2e.routing.spec.js
+++ taxcalculator/taxcalculator-presentation/src/test/js/e2e/e2e.routing.spec.js
@@ -8,12 +8,6 @@ describe('E2E: tests for the page routing', function() {
             browser().navigateTo('/taxcalculator/#/employees');
         });
 
-        it('should generate 1 employees', function() {
-            input('GenerateEmployeesModel.employeesCount').enter('1');
-            element('#generateEmployeesSubmit').click();
-            expect(element('[ng-view] table tr.employeeRow').count()).toBe(1);
-        });
-
         it('should generate 4 employees', function() {
             input('GenerateEmployeesModel.employeesCount').enter('4');
             element('#generateEmployeesSubmit').click();
