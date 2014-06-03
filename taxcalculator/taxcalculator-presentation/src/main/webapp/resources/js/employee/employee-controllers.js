35e3dce87c579ca866ba421e9ad6af607c23c8be
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-03 15:58:45 +0300

    [Monica, Andrei] split ugly big file in pretty small files

--- taxcalculator/taxcalculator-presentation/src/main/webapp/resources/js/employee/employee-controllers.js
+++ taxcalculator/taxcalculator-presentation/src/main/webapp/resources/js/employee/employee-controllers.js
@@ -1,4 +1,4 @@
-var taxcalculatorControllers = angular.module('taxcalculatorControllers', []);
+var taxcalculatorControllers = angular.module('employee.controllers', []);
 
 taxcalculatorControllers
 
@@ -89,31 +89,6 @@ taxcalculatorControllers
     }
   ])
 
-  .controller('JobResultsCtrl', ['$scope', 'JobResultsResource', 'RunJobResource',
-    function ($scope, JobResultsResource, RunJobResource) {
-      $scope.isReportReady = function (jobExecution) {
-        return jobExecution != undefined && jobExecution.status != undefined && (jobExecution.status == 'FAILED' || jobExecution.status == 'COMPLETED');
-      }
-
-      $scope.formatDuration = function (millis) {
-        if (millis === null) return "NOT FINISHED";
-        duration = moment.duration(millis);
-        return duration.hours() + "h " + duration.minutes() + "m " + duration.seconds() + "s " + duration.milliseconds() + "ms"
-      }
-      $scope.jobResults = JobResultsResource.query(
-        {},
-        function (successData) {
-        },
-        function (error) {
-          $scope.$emit("alert", {'alertClass': 'alert-danger', 'message': 'Could not start job'})
-        }
-      );
-
-      $scope.runJob = function (job) {
-        RunJobResource.run({year: job.jobStartParams.year, month: job.jobStartParams.month});
-      }
-    }
-  ])
   .controller('GenerateEmployeesCtrl', ['$scope', 'GenerateEmployeesResource', '$routeParams',
     function ($scope, GenerateEmployeesResource, $routeParams) {
       $scope.GenerateEmployeesModel = {employeesCount: $routeParams.employeesCount};
