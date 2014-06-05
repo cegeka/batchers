04d79f6364464a29158e9760d00f8517ab8cd7bf
Ronald Dehuysser <ronald.dehuysser@cegeka.be>
2014-06-05 14:43:51 +0200

    [Ronald] simulate bug #1

--- taxcalculator/taxcalculator-application/src/test/java/be/cegeka/batchers/taxcalculator/application/domain/pdf/PDFGeneratorServiceTest.java
+++ taxcalculator/taxcalculator-application/src/test/java/be/cegeka/batchers/taxcalculator/application/domain/pdf/PDFGeneratorServiceTest.java
@@ -8,7 +8,10 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.*;
 
 import static be.cegeka.batchers.taxcalculator.application.ApplicationAssertions.assertThat;
 
@@ -29,5 +32,49 @@ public class PDFGeneratorServiceTest {
                 .containsText("cegeka-batchers is working");
     }
 
+    @Test
+    public void pdfGeneratorServiceIsThreadSafe() throws InterruptedException, ExecutionException {
+        PDFGeneratorService pdfGeneratorService = new PDFGeneratorService();
+
+        int numberOfSimultaneousThreads = 5;
+
+        CountDownLatch startCountDownLatch = new CountDownLatch(1);
+        CountDownLatch endCountDownLatch = new CountDownLatch(numberOfSimultaneousThreads);
+
+        Set<Future<byte[]>> results = new HashSet<>();
+
+        ExecutorService pool = Executors.newFixedThreadPool(numberOfSimultaneousThreads);
+        for (int i = 0; i < numberOfSimultaneousThreads; i++) {
+            Callable<byte[]> pdfGeneratorServiceForMultipleThreads = createPdfGeneratorServiceForMultipleThreads(pdfGeneratorService, startCountDownLatch, endCountDownLatch);
+            results.add(pool.submit(pdfGeneratorServiceForMultipleThreads));
+        }
+
+        startCountDownLatch.countDown();
+        endCountDownLatch.await();
+
+        for (Future<byte[]> result : results) {
+            result.get();
+        }
+
+        assertThat("no exception has been thrown").isNotEmpty();
+    }
+
+    private Callable<byte[]> createPdfGeneratorServiceForMultipleThreads(final PDFGeneratorService pdfGeneratorService, CountDownLatch startCountDownLatch, CountDownLatch endCountDownLatch) {
+        return new Callable<byte[]>() {
+            @Override
+            public byte[] call() throws Exception {
+                try {
+                    startCountDownLatch.await();
+
+                    Map<String, Object> context = new HashMap<>();
+                    context.put("test", "cegeka-batchers");
+
+                    return pdfGeneratorService.generatePdfAsByteArray(new ClassPathResource("test_template.docx"), context);
+                } finally {
+                    endCountDownLatch.countDown();
+                }
+            }
+        };
+    }
 
 }
