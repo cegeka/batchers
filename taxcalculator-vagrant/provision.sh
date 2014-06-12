49a7f5dbd51c79fa51b051c8dc3be7ff1d676a05
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-12 17:25:59 +0300

    [Madalin, Andrei] convert windos line endings to UNIX line endings

--- taxcalculator-vagrant/provision.sh
+++ taxcalculator-vagrant/provision.sh
@@ -13,6 +13,11 @@ echo "127.0.0.1     taxcalculatordb" | tee -a /etc/hosts
 # make all files form scripts executable (windows removes this flag)
 chmod +x scripts/*.sh
 
+cd scripts
+find ./ -type f -exec sed -i -e 's/^M$//' {} \;
+find ./ -type f -exec sed -i -e $'s/\r$//' {} \;
+cd ..
+
 ./scripts/install_xvfb_and_firefox.sh
 
 ./scripts/install_oracle_java8.sh
@@ -22,4 +27,4 @@ chmod +x scripts/*.sh
 ./scripts/install_rabbitmq.sh
 
 #run script as Vagrant user not as root
-su -c "./scripts/install_as_vagrant.sh" -s /bin/bash vagrant
+sudo -u vagrant ./scripts/install_as_vagrant.sh
