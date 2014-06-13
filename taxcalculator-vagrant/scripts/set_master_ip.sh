011090efecde332aa144966ba3ef2f76eb6e6231
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-13 17:58:13 +0300

    [Andrei] better config for setting the host file, port forward for slave

--- taxcalculator-vagrant/scripts/set_master_ip.sh
+++ taxcalculator-vagrant/scripts/set_master_ip.sh
@@ -7,7 +7,6 @@ if [ -z "$BATCHERS_MASTER_IP" ]; then export BATCHERS_MASTER_IP="127.0.0.1" ;fi
 echo "BATCHERS_MASTER_IP is $BATCHERS_MASTER_IP"
 
 # delete the line containing batchersmaster in /etc/hosts
-echo "$BATCHERS_MASTER_IP     batchersmaster" | tee -a /etc/hosts
 sed -i '/batchersmaster/d' /etc/hosts
 echo "setting batchersmaster to $BATCHERS_MASTER_IP in /etc/hosts"
-echo "$BATCHERS_MASTER_IP	batchersmaster" | tee -a /etc/hosts
+echo "$BATCHERS_MASTER_IP     batchersmaster" | tee -a /etc/hosts
