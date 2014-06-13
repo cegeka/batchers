1d48a60de2db021694db09abd86712d5a4e594f8
madalinb <madalin.balan@cegeka.com>
2014-06-13 16:59:27 +0300

    added provision with variables

--- taxcalculator-vagrant/provision.sh
+++ taxcalculator-vagrant/provision.sh
@@ -1,33 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-export DEBIAN_FRONTEND=noninteractive
-apt-get update && apt-get upgrade -y
-apt-get install git software-properties-common -y
-
-
 # make all files form scripts executable (windows removes this flag)
 chmod +x scripts/*.sh
-
-echo "$USER BATCHERS_PROFILE is $BATCHERS_PROFILE"
-echo "$USER BATCHERS_MASTER_IP is $BATCHERS_MASTER_IP"
-
-./set_master_ip.sh
 
 cd scripts
 find ./ -type f -exec sed -i -e 's/^M$//' {} \;
 find ./ -type f -exec sed -i -e $'s/\r$//' {} \;
 cd ..
-
-export PATH="/home/vagrant/.local/bin/karma:$PATH"
-
-./scripts/install_xvfb_and_firefox.sh
-
-./scripts/install_oracle_java8.sh
-
-./scripts/install_mysql.sh
-
-./scripts/install_rabbitmq.sh
-
-#run script as Vagrant user not as root
-sudo -i -H -u vagrant ./scripts/install_as_vagrant.sh
