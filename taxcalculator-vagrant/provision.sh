f7fd23b36649c3e68203b2a74e188e6ac0c21a28
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-13 11:35:00 +0300

    [Andrei] added "batchersmaster" as a central point for the master host resolution

--- taxcalculator-vagrant/provision.sh
+++ taxcalculator-vagrant/provision.sh
@@ -8,7 +8,7 @@ apt-get install git -y
 # delete the line containing taxcalculatordb etc/hosts
 sed -i '/taxcalculatordb/d' /etc/hosts
 # set master database ip address
-echo "127.0.0.1     taxcalculatordb" | tee -a /etc/hosts
+echo "127.0.0.1     batchersmaster" | tee -a /etc/hosts
 
 # make all files form scripts executable (windows removes this flag)
 chmod +x scripts/*.sh
