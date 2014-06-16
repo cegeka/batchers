adbb7572baac40ada89e35bf6d2f60827bf45230
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-16 18:36:07 +0300

    [Andrei] fixed multiple jobs synchronization problem and added logger

--- taxcalculator/taxcalculator-presentation/src/main/java/be/cegeka/batchers/taxcalculator/presentation/rest/controller/JobRestController.java
+++ taxcalculator/taxcalculator-presentation/src/main/java/be/cegeka/batchers/taxcalculator/presentation/rest/controller/JobRestController.java
@@ -13,24 +13,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Controller
 public class JobRestController {
     private static final Logger LOG = LoggerFactory.getLogger(JobRestController.class);
     @Autowired
-    JobService jobService;
+    private JobService jobService;
+
+    private Lock lock = new ReentrantLock();
 
     @RequestMapping(value = "runJob/{year}/{month}", method = RequestMethod.POST)
     @ResponseBody
     public void runJob(@PathVariable("year") Long year, @PathVariable("month") Long month) {
-        LOG.debug("Running job in rest controller");
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        jobService.runTaxCalculatorJob(new JobStartParams(year, month));
-        stopwatch.stop();
+        if(lock.tryLock()){
+          try{
+            LOG.debug("Running job in rest controller");
+            Stopwatch stopwatch = Stopwatch.createStarted();
+            jobService.runTaxCalculatorJob(new JobStartParams(year, month));
+            stopwatch.stop();
 
-        System.out.println("=======================================");
-        System.out.println("Time needed: " + stopwatch.elapsed(TimeUnit.SECONDS));
-        System.out.println("=======================================");
+            LOG.info("=======================================");
+            LOG.info("Time needed: " + stopwatch.elapsed(TimeUnit.SECONDS));
+            LOG.info("=======================================");
+          } finally {
+            lock.unlock();
+          }
+        } else {
+          LOG.info("A job is currently running. Try again later.");
+        }
     }
 
 }
