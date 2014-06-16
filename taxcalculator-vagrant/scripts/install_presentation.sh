39732ed9420aa0ca1e5cf8c90035b85037f351ee
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-16 11:59:34 +0300

    [Andrei] added master slave for tomcat and intellij project

--- taxcalculator-vagrant/scripts/install_presentation.sh
+++ taxcalculator-vagrant/scripts/install_presentation.sh
@@ -14,7 +14,17 @@ sed -i 's/"8080"/"9090"/g' apache-tomcat-7.0.54-presentation/conf/server.xml
 rm -rf apache-tomcat-7.0.54/webapps/taxcalculator-*
 cp batchers/taxcalculator/taxcalculator-presentation/target/taxcalculator-presentation-1.0-SNAPSHOT.war apache-tomcat-7.0.54-presentation/webapps/taxcalculator.war
 
-echo "export JAVA_OPTS=\"-DAPP_ENV=staging\"" | tee apache-tomcat-7.0.54-presentation/bin/setenv.sh
+MY_JAVA_OPTS="export JAVA_OPTS=\"-DAPP_ENV=staging\""
+if [ $BATCHERS_PROFILE = "master" ]
+then
+    MY_JAVA_OPTS="export JAVA_OPTS=\"-Dspring.profiles.active=remotePartitioningMaster -DAPP_ENV=staging\""
+fi
+if [ $BATCHERS_PROFILE = "slave" ]
+then
+    MY_JAVA_OPTS="export JAVA_OPTS=\"-Dspring.profiles.active=remotePartitioningSlave -DAPP_ENV=staging \""
+fi
+
+echo $MY_JAVA_OPTS | tee apache-tomcat-7.0.54-presentation/bin/setenv.sh
 chmod +x apache-tomcat-7.0.54-presentation/bin/setenv.sh
 
 apache-tomcat-7.0.54-presentation/bin/catalina.sh start
