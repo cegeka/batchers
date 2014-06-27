d49c4769bee28de5e0c8b27e3cb3192da4228b8c
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-27 11:00:22 +0300

    [Andrei] added stub web service simple UI http://localhost:9091/stubwebservice/taxservice

--- taxcalculator/taxcalculator-stubwebservice/src/main/java/be/cegeka/batchers/taxservice/stubwebservice/TextFileTaxLogger.java
+++ taxcalculator/taxcalculator-stubwebservice/src/main/java/be/cegeka/batchers/taxservice/stubwebservice/TextFileTaxLogger.java
@@ -1,15 +1,33 @@
 package be.cegeka.batchers.taxservice.stubwebservice;
 
+import com.google.common.collect.EvictingQueue;
+import com.google.common.collect.Queues;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Queue;
 
 @Component
 public class TextFileTaxLogger {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TextFileTaxLogger.class);
 
+    private final Queue<String> logs = Queues.synchronizedQueue(EvictingQueue.create(100));
+
     public void log(Long employeeId, double taxAmount, String status) {
-        LOGGER.info("Received tax request for employee with id " + employeeId + " with amount " + taxAmount + " and completed with status: " + status);
+        String msg = "Received tax request for employee with id " + employeeId + " with amount " + taxAmount + " and completed with status: " + status;
+        logs.add(msg);
+        LOGGER.info(msg);
+    }
+
+    public String getLogs(){
+        StringBuilder sb = new StringBuilder();
+        logs.stream().forEach(line -> sb.append(line).append("\n"));
+        return sb.toString();
+    }
+
+    public void clear(){
+        logs.clear();
     }
 }
