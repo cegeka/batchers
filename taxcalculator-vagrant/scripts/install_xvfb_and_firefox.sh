c731ec020d5ae8537ed492ab31a990da73a2a1a0
Andrei Cristian Petcu <andrei@ceata.org>
2015-03-01 16:58:56 +0200

    soft link for chrome/chromium

--- taxcalculator-vagrant/scripts/install_xvfb_and_firefox.sh
+++ taxcalculator-vagrant/scripts/install_xvfb_and_firefox.sh
@@ -3,6 +3,7 @@ set -e
 
 # install firefox xvfb for e2e headless tests
 apt-get install firefox chromium-browser xvfb -y
+rm -rf /usr/bin/chrome && ln -s /usr/bin/chromium-browser /usr/bin/chrome
 
 # configure display for e2e
 if [ -f /etc/profile.d/batchers.sh ]; then source /etc/profile.d/batchers.sh ;fi
