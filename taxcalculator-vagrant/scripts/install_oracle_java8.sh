d6fa1c9f5e5a90628d7eef5c1836833632d9dc47
Andrei Petcu <andreicristianpetcu@gmail.com>
2015-06-18 14:18:48 +0300

    Ignore unknown GPG key!!

--- taxcalculator-vagrant/scripts/install_oracle_java8.sh
+++ taxcalculator-vagrant/scripts/install_oracle_java8.sh
@@ -5,7 +5,7 @@ add-apt-repository ppa:webupd8team/java -y
 # accept java license with no user interaction
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get update
-sudo apt-get install oracle-java8-installer -y
+sudo apt-get install oracle-java8-installer -y --force-yes
 # do not install maven before java because it will install openjdk7
 sudo apt-get install maven -y
 # set java8 as default
